f2c3473dac7a4754d2ca6247e14a78344ca9fcd0
inventivetalent <mail@inventivetalent.org>
2021-02-13 11:13:43 +0100

    support for mongo url

--- db/db.js
+++ db/db.js
@@ -1,8 +1,13 @@
 module.exports = function (mongoose, config) {
 
     // Connect to DB
-    console.log("Connecting to mongodb://" + ((config.mongo.user || "admin") + ":*****" + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")))
-    mongoose.connect("mongodb://" + ((config.mongo.user || "admin") + ":" + (config.mongo.pass || "admin") + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
+    if (config.mongo.url) {
+        console.log("Connecting to mongo...");
+        mongoose.connect(config.mongo.url);
+    } else {
+        console.log("Connecting to mongodb://" + ((config.mongo.user || "admin") + ":*****" + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")))
+        mongoose.connect("mongodb://" + ((config.mongo.user || "admin") + ":" + (config.mongo.pass || "admin") + "@" + (config.mongo.address || "localhost") + ":" + (config.mongo.port || 27017) + "/" + (config.mongo.database || "database")));
+    }
 
     mongoose.Promise = Promise;
 }
